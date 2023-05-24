21a079328e6a5d4731f0a302f6ab74ac3d0d9302
Andrey210589 <PolishhukAk@mail.ru>
2023-05-25 02:07:03 +0300

    Init_commit

---
+++ Task1.py
@@ -0,0 +1,29 @@
+
+# Задача 10: 
+# На столе лежат n монеток. 
+# Некоторые из них лежат вверх решкой, 
+# а некоторые – гербом. 
+# Определите минимальное число монеток,
+# которые нужно перевернуть, чтобы все монетки 
+# были повернуты вверх одной и той же стороной. 
+# Выведите минимальное количество монет, 
+# которые нужно перевернуть
+
+
+# n=int(input("Input the number of coins "))
+# count=0
+# sum = 0
+# t=0
+# while count<n:
+#     h=int(input("If coin side is heads input 1 else input 0 "))
+#     while h>1 or h<0:
+#         h=int(input("Try again. If heads input 1 else input 0 "))
+#     sum+=h
+#     count+=1
+# t=n-sum
+# if sum<=t:
+#     print(f"Minimally need to flip {sum} heads")
+# else:
+#     print(f"Minimally need to flip {t} tails")
+
+
